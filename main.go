ef89359771695e8a9507a00cbb191a666f0cae4a
Joel Maritnez <j_m803@txstate.edu>
2019-03-22 14:09:05 -0500

    Updated main.go
    main.go now accepts number of debts, allows the user to give a name to the debt, and the amount of the debt

--- main.go
+++ main.go
@@ -1,13 +1,15 @@
 package main
 
-import "fmt"
-
-///////////////////////////////////////////////////////////////////////////////
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 ///////////////////////////////////////////////////////////////////////////////
 type debt struct {
 	name   string
-	amount float32
+	amount float64
 }
 
 func (d debt) getName() {
@@ -18,8 +20,45 @@ func (d debt) getAmountOwed() {
 }
 
 ///////////////////////////////////////////////////////////////////////////////
+func printMenu() {
+	var numDebts int
+	var debtAmt float64
+	debtSlice := make([]debt, 0)
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Println("--------WELCOME TO YOUR DEBT TRACKER---------")
+	fmt.Println("---------------------------------------------")
+	fmt.Println()
+	fmt.Println("How many debts do you have? ")
+	fmt.Scanln(&numDebts)
+	for x := 1; x <= numDebts; x++ {
+		fmt.Printf("What would you like to name debt #%d?\n", x)
+		debtName, _ := reader.ReadString('\n')
+		fmt.Printf("How much do you owe for debt #%d?\n", x)
+		fmt.Scanln(&debtAmt)
+		d := debt{debtName, debtAmt}
+		debtSlice = append(debtSlice, d)
+	}
+	fmt.Println()
+	fmt.Println(debtSlice)
+}
+
+func addDebts(nums ...float64) float64 {
+	totalDebt := 0.0
+
+	for _, x := range nums {
+		totalDebt += x
+	}
+
+	return totalDebt
+}
+
+///////////////////////////////////////////////////////////////////////////////
 func main() {
-	d := debt{"School Loans", 5000}
-	d.getName()
-	d.getAmountOwed()
+	printMenu()
+
+	//nums := []float64{1200, 3000.75, 3500}
+	//fmt.Println(addDebts(nums...))
+
+	os.Exit(1)
 }
